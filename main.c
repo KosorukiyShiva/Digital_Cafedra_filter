38b71ea091b83da11cc712348170870ffe7a5a21
Shiva_win <alcatelandry9@gmail.com>
2025-02-06 16:45:50 +0300

    started realisation of meandr function

--- main.c
+++ main.c
@@ -3,21 +3,36 @@
 #include <stdlib.h>
 
 #define t 100 // количество итераций
+#define turn_on_meandr 1
+#define turn_on_sine 1
 
-float meandr_(float x);
-float sine_(float x);
+float meandr_(float x, float *arr);
+float sine_(float x, float *arr);
 int main(){
-    float *meandr_vector = calloc(t,sizeof(float));
-    float *sine_vector = calloc(t,sizeof(float));
-
+    if(turn_on_meandr == 1){
+        float *meandr_vector = calloc(t,sizeof(float));
+        meandr_(t, meandr_vector);
+    }
+    if(turn_on_sine == 1){
+        float *sine_vector = calloc(t,sizeof(float));
+        sine_(t, sine_vector);
+    }
+    return 0;
 }
-float meandr_(float x){
+float meandr_(float x, float *arr){
     float y = 0;
+    for (float  i = 0; i < x; i++)
+    {
+        int j = i;
+        y = i - ((i*i*i)/(6))+ ((i*i*i*i*i)/(120))-((i*i*i*i*i*i*i)/(5040));
+        arr[j] = y;
+        y = 0;
+    }
     
-
+    
 }
 
-float sine_(float x){
+float sine_(float x, float *arr){
     float y = 0;
 
 }

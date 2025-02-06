7e5aeeaa24ce8a09a1656d387a35468485137434
Shiva_win <alcatelandry9@gmail.com>
2025-02-06 14:41:16 +0300

    added prortotypes

--- main.c
+++ main.c
@@ -1,14 +1,23 @@
 #include <math.h>
 #include <stdio.h>
-void meandr_();
-void sine_();
+#include <stdlib.h>
+
+#define t 100 // количество итераций
+
+float meandr_(float x);
+float sine_(float x);
 int main(){
+    float *meandr_vector = calloc(t,sizeof(float));
+    float *sine_vector = calloc(t,sizeof(float));
 
 }
-void meandr_(){
+float meandr_(float x){
+    float y = 0;
+    
 
 }
 
-void sine_(){
+float sine_(float x){
+    float y = 0;
 
 }
